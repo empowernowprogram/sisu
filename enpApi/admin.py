dc8b0d84b4568dcd53c8db22cbd30fec882999e4
yoshuanl <60448330+yoshuanl@users.noreply.github.com>
2021-06-16 18:35:25 -0700

    add models

--- enpApi/admin.py
+++ enpApi/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from .models import Player, Employer, PlaySession, ModuleDownloadLink
+from .models import Player, Employer, PlaySession, ModuleDownloadLink, ComparisonRating, Adjective, SelectedAdjective, PostProgramSurvey
 
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('user', 'employer', 'supervisor', 'admin')
@@ -20,8 +20,24 @@ class ModuleDownloadLinkAdmin(admin.ModelAdmin):
     list_display = ('id', 'training_type', 'platform_category', 'is_supervisor')
 
 
+class PostProgramSurveyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'has_completed', 'overall_experience', 'creation_date')
+
+class AdjectiveAdmin(admin.ModelAdmin):
+    list_display = ('adj_id', 'description')
+
+class ComparisonRatingAdmin(admin.ModelAdmin):
+    list_display = ('comparison_rating_id', 'description')
+
+class SelectedAdjectiveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'adj_id')
+
 
 admin.site.register(Player, PlayerAdmin)
 admin.site.register(Employer, EmployerAdmin)
 admin.site.register(PlaySession, PlaySessionAdmin)
 admin.site.register(ModuleDownloadLink, ModuleDownloadLinkAdmin)
+admin.site.register(PostProgramSurvey, PostProgramSurveyAdmin)
+admin.site.register(Adjective, AdjectiveAdmin)
+admin.site.register(ComparisonRating, ComparisonRatingAdmin)
+admin.site.register(SelectedAdjective, SelectedAdjectiveAdmin)
