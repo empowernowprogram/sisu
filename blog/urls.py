962a25d6cb5453e3e82873fb53682c14a5260f15
Robert Miller <robbymilrr@gmail.com>
2021-07-02 10:41:17 -0700

    Revert "Login portal updates"
    This reverts commit c34fa6cb8baa2f78e9061d3fc931e10df8241483.
    
    Lots of cleanup of stale functions/views/pages
    
    General improvements and updates
    
    Login portal updates
    
    - Updated href to be dynamic using url "name-of-page"
    - Updated copyright to be 2021 (todo - better method is to have it automatically update/change)
    
    collectstatic
    
    minor change
    
    Revert "collectstatic"
    
    This reverts commit 64c0fa1a7dab36301d2713da887dc3bedafa65fc.

--- blog/urls.py
+++ blog/urls.py
@@ -4,30 +4,32 @@ from . import views
 #from .views import IndexView
 
 urlpatterns = [
+<<<<<<< HEAD
+    # Public website - START
     path('', views.index, name='home' ),    
+=======
+    path('', views.index, name='index' ),
+    # url(r'^$', views.about_sisu, name='about'),
+    
+>>>>>>> parent of d933068 (General cleanup)
     path('about-us/', views.about_us, name='about-us'),
 
-    path('about_us/team', views.about_team, name='about_team'),
-    path('about_us/empower_now', views.about_program, name='about_program'),
-    path('post/', views.post_list, name='post_list'),
+    
     path('post/new/', views.post_new, name='post_new'),
-    path('drafts/', views.post_draft_list, name='post_draft_list'),
-    path('post/(?P<pk>\d+)/publish/', views.post_publish, name='post_publish'),
-    path('post/(?P<pk>\d+)/remove/', views.post_remove, name='post_remove'),
-    path('post/(?P<pk>\d+)/edit/', views.post_edit, name='post_edit'),
-    path('comment/(?P<pk>\d+)/approve/', views.comment_approve, name='comment_approve'),
-    path('comment/(?P<pk>\d+)/remove/', views.comment_remove, name='comment_remove'),
-    path('post/category/(?P<category_name>\w+)', views.post_list_by_category, name='post_list_by_category'),
+    path('post/(<pk>\d+)/publish/', views.post_publish, name='post_publish'),
+    path('post/(<pk>\d+)/remove/', views.post_remove, name='post_remove'),
+    path('post/(<pk>\d+)/edit/', views.post_edit, name='post_edit'),
+    path('comment/(<pk>\d+)/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/(<pk>\d+)/remove/', views.comment_remove, name='comment_remove'),
+    path('post/category/(<category_name>\w+)', views.post_list_by_category, name='post_list_by_category'),
     path('cases', views.post_cases, name='post_cases'),
-    path('contact_us', views.contact_us, name='contact_us'),
-    path('sisu_case/category/(?P<category_name>\w+)', views.story, name='story'),
-    path('sisu_case/(?P<pk>\d+)/', views.story_entry, name='story_entry'),
-    path('user_details/user_id/(?P<pk>\d+)/', views.user_details, name='user_details'),
+    path('sisu_case/category/(<category_name>\w+)', views.story, name='story'),
+    path('sisu_case/(<pk>\d+)/', views.story_entry, name='story_entry'),
+    path('user_details/user_id/(<pk>\d+)/', views.user_details, name='user_details'),
     path('likepost/', views.on_off_star, name='on_off_star'),
     path('add_comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('add_reply_to_comment', views.add_reply_to_comment, name='add_reply_to_comment'),
     path('search', views.search, name='search'),
-    #url(r'^settings/user_id(?P<pk>\d+)/$', IndexView.as_view(), name='user_details'),
 
     path('sisu_case/category/', views.get_all_category, name='get_all_category'),
     path('enp_login/', views.login_portal, name='login_portal'),
@@ -40,19 +42,15 @@ urlpatterns = [
     path('downloads_s/', views.downloads_s, name='downloads_s'),
     path('employee_reg', views.employee_reg, name='employee_reg'),
     path('send_reg/', views.send_reg, name='send_reg'),
+    
     path('reg_from_invite', views.reg_from_invite, name='reg_from_invite'),
-
     path('terms_conditions/', views.terms_conditions, name='t_c'),
     path('privacy_policy/', views.privacy_policy, name='p_p'),
-
-    path('header/', views.header, name='header'),
-
-
-    # PUBLIC SITE - START
     path('faq', views.faq, name='faq'),
     path('contact', views.contact, name='contact'),
-    # PUBLIC SITE - END
+    # Public website - END
     
+
     # Training Portal Authentication / Login / Logout - START
     path('auth-login/', views.portal_login, name='portal_login'),
     path('auth-login-trial/', views.portal_login_trial, name='portal_login_trial'),
