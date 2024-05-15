ebcb9bb768553737e5fcaec661a4c2d4197029a3
Sean Rossi <srossi455@gmail.com>
2024-05-15 08:46:50 -0700

    Updated settings for HTTPS redirect. Updated ENP MG and NoCode videos

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,7 @@ import cloudinary.api
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', cast=bool)
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 #ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
 DEBUG = True
